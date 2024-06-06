7c441b2bcef5ea82cdc2c7af5473a1f5dbe5f4cc
YiHsuanZhang <yihsuan.chang@outlook.com>
2024-06-06 16:33:51 +0800

    addcshtml

---
+++ DanGame/Models/GameViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DanGame.Models
+{
+    public class GameViewModel
+    {
+        public App App { get; set; }
+        public AppDetail AppDetail { get; set; }
+        public GenreTag GenreTag { get; set; }
+    }
+}
