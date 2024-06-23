ffe48ab83c64b8b1378d8088da6e63aa71f4a3f1
YuHansage <sageyuhan@gmail.com>
2024-06-23 16:48:42 +0800

    好友字卡新增資訊 好友動態API更新 玩家遊戲庫(未完成)

---
+++ DanGame/Controllers/GamelibraryController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DanGame.Models;
+
+namespace DanGame.Controllers
+{
+    public class GamelibraryController : Controller
+    {
+        private DanGameContext _context;
+
+        public GamelibraryController(DanGameContext dbContext)
+        {
+            _context = dbContext;
+        }
+        public IActionResult Index()
+        {
+			var id = Request.HttpContext.Session.GetInt32("UserId");
+            var orders = _context.Orders
+                         .Where(order => order.UserId == id)
+                         .Select(order => order.OrderItems.Select(oi => new { oi.AppId, oi.App.AppName })).ToList();
+
+			return View(orders.SelectMany(a => a));
+		}
+    }
+}
