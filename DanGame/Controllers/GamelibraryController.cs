69f93273d87564dc120457b6ac405b1945a03542
YuHansage <sageyuhan@gmail.com>
2024-06-23 17:28:13 +0800

    玩家遊戲庫(完成)

--- DanGame/Controllers/GamelibraryController.cs
+++ DanGame/Controllers/GamelibraryController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using DanGame.Models;
+using System.Linq;
 
 namespace DanGame.Controllers
 {
@@ -14,11 +15,29 @@ namespace DanGame.Controllers
         public IActionResult Index()
         {
 			var id = Request.HttpContext.Session.GetInt32("UserId");
-            var orders = _context.Orders
-                         .Where(order => order.UserId == id)
-                         .Select(order => order.OrderItems.Select(oi => new { oi.AppId, oi.App.AppName })).ToList();
+			//         var orders = _context.Orders
+			//                      .Where(order => order.UserId == id)
+			//                      .Select(order => order.OrderItems.Select(oi => new { oi.AppId, oi.App.AppName }))
+			//			 .ToList();
 
-			return View(orders.SelectMany(a => a));
+			//return View(orders.SelectMany(a => a));
+			var orderItems = _context.Orders
+					 .Where(order => order.UserId == id)
+					 .SelectMany(order => order.OrderItems.Select(oi => new { oi.AppId, oi.App.AppName }))
+					 .ToList();
+			//排除重複的App
+			var uniqueOrderItems = new List<object>();
+			var appIdSet = new HashSet<int>();
+
+			foreach (var item in orderItems)
+			{
+				if (appIdSet.Add(item.AppId))
+				{
+					uniqueOrderItems.Add(item);
+				}
+			}
+
+			return View(uniqueOrderItems);
 		}
     }
 }
